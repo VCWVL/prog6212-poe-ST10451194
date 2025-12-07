dd30d616de5b0a83e83cac608c170e471bcef136
ST10451194 <166477035+ST10451194@users.noreply.github.com>
2025-12-07 16:45:56 +0200

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace PROG_POE.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
